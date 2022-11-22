3b9bf22940759f88d08ed6b3da8ae582e2d150ff
PuruTonik <107607138+PuruTonik@users.noreply.github.com>
2022-11-22 17:28:51 +0530

    Add files via upload
    Nodejs for promoter app service

---
+++ README.md
@@ -0,0 +1,9 @@
+# serverless-api-tutorial-code
+
+This repo contains the lambda code for my YouTube tutorial (https://www.youtube.com/watch?v=Ut5CkSz6NR0) on how to build a serverless api with AWS lambda, API gateway and a dynamodb from scratch. Step by step instructions are provided.
+npm install --save amazon-cognito-identity-js
+npm install --save aws-sdk
+npm install --save request
+npm install --save jwk-to-pem
+npm install --save jsonwebtoken
+npm install --save node-fetch
---
+++ index.js
@@ -0,0 +1,512 @@
+//import { createHash } from 'node:crypto'
+const AWS = require('aws-sdk');
+var crypto = require('crypto');
+AWS.config.update({
+  region: 'ap-southeast-1'
+});
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dynamodbTableName = 'TDBK_BNPL_PRODUCT_DTL';
+const promotersTableName = 'PROMOTERS_USER_DTL';
+const healthPath = '/health';
+const productPath = '/product';
+const promoterPath = '/promoters/user';
+const promoterloginPath = '/promoters/login';
+const NO_RECORD_FOUND = 'No Data Found';
+const INVALID_REQ = 'invalid request payload';
+const EXPIRED = 'Request expired';
+const USED = 'Product purchased';
+const MIN_DIFF = 30;
+const MAX_ATTEMP= 5;
+exports.handler = async function (event) {
+  console.log('Request event: ', event.httpMethod);
+  let response;
+  switch (true) {
+    case event.httpMethod === 'GET' && event.path === healthPath:
+      response = buildResponse(200);
+      break;
+    case event.httpMethod === 'GET' && event.path === productPath:
+      response = await getProduct(event.queryStringParameters.productId);
+      break;
+    case event.httpMethod === 'POST' && event.path === productPath:
+      response = await saveProduct(JSON.parse(event.body));
+      break;
+    case event.httpMethod === 'PATCH' && event.path === productPath:
+      const requestBody = JSON.parse(event.body);
+      response = await modifyProduct(requestBody.id, requestBody.loanId, requestBody.digitalUserId, requestBody.status);
+      break;
+    case event.httpMethod === 'POST' && event.path === promoterPath:
+      response = await createUser(JSON.parse(event.body));
+      break;
+    case event.httpMethod === 'POST' && event.path === promoterloginPath:
+      response = await validateLogin(JSON.parse(event.body));
+      let verifyloginResp = JSON.parse(response.body);
+      if(verifyloginResp.status==="x07")
+      response = await wrongAttempt(JSON.parse(event.body),response)
+      break;
+    case event.httpMethod === 'PATCH' && event.path === promoterPath:
+        response = await verifyUser(JSON.parse(event.body),response);
+       // console.log("Verify User Resp :"+JSON.stringify(response))
+       let value = JSON.parse(response.body);
+       if(value.status==="00")
+        response = await changePwd(JSON.parse(event.body),response)
+        break;
+    default:
+      response = buildResponse(404, NO_RECORD_FOUND);
+  }
+  return response;
+}
+
+async function getProduct(productId) {
+
+  if (Object.keys(productId).length === 0)
+    return buildResponse(400, "Bad Request");
+  const params = {
+    TableName: dynamodbTableName,
+    Key: {
+      'id': productId
+    }
+  }
+  return await dynamodb.get(params).promise().then((response) => {
+    const body = {
+      operation: 'FETCH',
+      status: '00',
+      message: 'SUCCESS',
+      data: response.Item
+    }
+    if (Object.keys(response).length === 0)
+      return buildResponse(200, NO_RECORD_FOUND);
+    if (MIN_DIFF < getMinutesBetweenDates(new Date(response.Item.createdOn), new Date())) {
+      body.status = "x01";
+      body.message = EXPIRED;
+      body.data = null;
+      return buildResponse(200, body);
+    }
+    if (response.Item.status === 'U') {
+      body.status = "x02";
+      body.message = USED;
+      body.data = null;
+      return buildResponse(200, body);
+    }
+    return buildResponse(200, body);
+  }, (error) => {
+    console.error('Unable to get the product details: ', error);
+  });
+}
+
+async function saveProduct(requestBody) {
+  const timestamp = new Date().getTime();
+
+  const reqbody = {
+    id: requestBody.promoter.id.concat(timestamp),
+    createdOn: timestamp,
+    createdBy: requestBody.promoter.name,
+    status: "I",
+    digitalUserId: "",
+    loanId: "",
+    downPaymentAmount: requestBody.downPaymentAmount,
+    tenureId: requestBody.tenureId,
+    tenure: requestBody.tenure,
+    loanableAmount: requestBody.loanableAmount,
+    monthlyAmount: requestBody.monthlyAmount,
+    total: requestBody.total,
+    promoter: {
+      id: requestBody.promoter.id,
+      name: requestBody.promoter.name,
+      purpleKey: requestBody.promoter.purpleKey,
+    },
+    categories: requestBody.categories
+
+  }
+
+  console.log(reqbody);
+  const params = {
+    TableName: dynamodbTableName,
+    Item: reqbody
+  }
+  return await dynamodb.put(params).promise().then(() => {
+    const body = {
+      operation: 'SAVE',
+      status: '00',
+      message: 'SUCCESS',
+      data: reqbody
+    }
+    return buildResponse(201, body);
+  }, (error) => {
+    console.error('Unable to save the product details ', error);
+  })
+}
+
+async function modifyProduct(id, loanid, digitalId, statusVal) {
+  if (isEmpty(id) || isEmpty(loanid) || isEmpty(digitalId) || isEmpty(statusVal) || statusVal != 'U')
+    return buildResponse(400, INVALID_REQ);
+  const params = {
+    TableName: dynamodbTableName,
+    Key: {
+      'id': id
+    },
+    UpdateExpression: `set loanId = :value1 ,digitalId = :value2 ,#st = :value3`,
+    ExpressionAttributeValues: {
+      ':value1': loanid,
+      ':value2': digitalId,
+      ':value3': statusVal,
+    },
+    ExpressionAttributeNames: {
+      "#st": "status",
+    },
+    ReturnValues: 'UPDATED_NEW'
+  }
+  return await dynamodb.update(params).promise().then((response) => {
+    const body = {
+      operation: 'UPDATE',
+      status: '00',
+      message: 'SUCCESS',
+      data: response.Attributes
+    }
+    return buildResponse(200, body);
+  }, (error) => {
+    console.error('failed to update the status', error);
+  })
+}
+async function createUser(requestBody) {
+  const timestamp = new Date().getTime();
+
+  const reqbody = {
+    id:"P"+requestBody.location+ requestBody.userId,
+    createdOn: timestamp,
+    createdBy: requestBody.createdBy,
+    status: "A", // A = Active , L=Locked , 
+    pwdStatus: "F", //Force Pwd change flag ( F = Force pwd , A = Active , R=Reset pwd , E=expired)
+    pwdExpireDt: '',
+    pwdChangedDt: timestamp,
+    pwd: makePwd(8),
+    statuschangedDt: '',
+    updatedOn: '',
+    updatedBy: '',
+    lastName: requestBody.lastName,
+    firstName: requestBody.firstName,
+    middleName: requestBody.middleName,
+    dob: requestBody.dob, //MM/DD/YYYY
+    address: requestBody.address,
+    zipcode: requestBody.zipcode,
+    province: requestBody.province,
+    city: requestBody.city,
+    barangay: requestBody.barangay,
+    mobile: requestBody.mobile,
+    area: requestBody.area,
+    location:requestBody.location,
+    storeId: requestBody.storeId,
+    retryCt: 0 // Pwd retry count
+
+  }
+  if (isEmpty(requestBody.userId) || isEmpty(requestBody.lastName) || isEmpty(requestBody.firstName) || isEmpty(requestBody.address) || isEmpty(requestBody.zipcode) ||
+    isEmpty(requestBody.city) || isEmpty(requestBody.barangay) || isEmpty(requestBody.mobile) || isEmpty(requestBody.storeId) || isEmpty(requestBody.area) || !validateDateFormat(requestBody.dob))
+    return buildResponse(400, "Bad Request");
+
+  console.log(reqbody);
+  const params = {
+    TableName: promotersTableName,
+    Item: reqbody
+  }
+  const respparams = {
+    userId: "P"+requestBody.location+ requestBody.userId
+
+  }
+
+  return await dynamodb.put(params).promise().then(() => {
+    const body = {
+      operation: 'USER_CREATION',
+      status: '00',
+      message: 'SUCCESS',
+      data: respparams
+    }
+    return buildResponse(201, body);
+  }, (error) => {
+    console.error('Unable to create promoter details ', error);
+  })
+}
+async function validateLogin(requestBody) {
+  const timestamp = new Date().getTime();
+  console.log("Login:",requestBody);
+
+  if (isEmpty(requestBody.userId) || isEmpty(requestBody.password))
+    return buildResponse(400, "Bad Request");
+
+
+  const respparams = {
+    userId: requestBody.userId,
+    lastName: '',
+    firstName: '',
+    storeId: ''
+
+  }
+  const paramId = {
+    TableName: promotersTableName,
+    Key: {
+      'id': requestBody.userId
+    }
+  }
+  console.log("checking user details in db"+respparams);
+  return await dynamodb.get(paramId).promise().then((response) => {
+    //console.log("DB Pwd :"+JSON.stringify(response));
+    //console.log("Req Pwd :"+requestBody.password);
+    //console.log("Sizein DB :"+Object.keys(response).length);
+    if (Object.keys(response).length > 0 && response.Item.pwd === requestBody.password && response.Item.status==="A" && response.Item.pwdStatus!="F") {
+      respparams.lastName = response.Item.lastName;
+      respparams.firstName = response.Item.firstName;
+      respparams.storeId = response.Item.storeId;
+      const body = {
+        operation: 'VALIDATE_USER',
+        status: '00',
+        message: 'SUCCESS',
+        data: respparams
+      }
+      return buildResponse(200, body);
+    } 
+    else if(Object.keys(response).length > 0 && response.Item.status!="A")
+    {
+      const body = {
+        operation: 'VALIDATE_USER',
+        status: 'x04',
+        message: 'Your account has been locked, please contact admin',
+
+      }
+      return buildResponse(200, body);
+    }
+    else if(Object.keys(response).length > 0 && response.Item.pwd === requestBody.password && response.Item.pwdStatus==="F")
+    {
+      const body = {
+        operation: 'VALIDATE_USER',
+        status: 'x05',
+        message: 'Please change your temporary password',
+
+      }
+      return buildResponse(200, body);
+    }
+     else if(Object.keys(response).length > 0 && response.Item.pwd != requestBody.password)
+    {
+      const body = {
+        operation: 'VALIDATE_USER',
+        status: 'x07',
+        message: 'Please enter a valid username and password',
+        data:response
+
+      }
+       //wrongAttempt(response);
+      return buildResponse(200, body);
+    }
+    else {
+      const body = {
+        operation: 'VALIDATE_USER',
+        status: 'x03',
+        message: 'Please enter a valid username and password',
+
+      }
+      
+      return buildResponse(200, body);
+    }
+
+
+  }, (error) => {
+    console.error('Unable to create promoter details ', error);
+  })
+}
+async function verifyUser(requestBody) {
+  const timestamp = new Date().getTime();
+  console.log("Login:",requestBody);
+
+  if (isEmpty(requestBody.userId) || isEmpty(requestBody.password))
+    return buildResponse(400, "Bad Request");
+
+  const paramId = {
+    TableName: promotersTableName,
+    Key: {
+      'id': requestBody.userId
+    }
+  }
+  ///console.log("checking user details in db"+respparams);
+  return await dynamodb.get(paramId).promise().then((response) => {
+   
+    if (Object.keys(response).length > 0 && response.Item.status==="A") {
+    
+      response.Item.pwd=requestBody.password;
+      response.Item.pwdChangedDt=timestamp;
+      response.Item.pwdStatus="A";
+      response.Item.updatedBy=response.Item.lastName;
+      response.Item.updatedOn=timestamp;
+      response.Item.retryCt=0;
+      response.Item.status="A";
+      const body = {
+        operation: 'VALIDATE_USER',
+        status: '00',
+        message: 'SUCCESS',
+        data: response
+      }
+      
+      return buildResponse(200, body);
+      
+    } 
+    else if(Object.keys(response).length > 0 && response.Item.status==="L")
+    {
+      const body = {
+        operation: 'VALIDATE_USER',
+        status: 'x04',
+        message: 'Your account has been locked, please contact admin',
+
+      }
+      return buildResponse(200, body);
+    }
+    else if(Object.keys(response).length > 0 && response.Item.pwdStatus==="F")
+    {
+      const body = {
+        operation: 'VALIDATE_USER',
+        status: 'x05',
+        message: 'Please change password',
+
+      }
+      return buildResponse(200, body);
+    }
+    
+    else {
+      const body = {
+        operation: 'VALIDATE_USER',
+        status: 'x03',
+        message: 'Please share valid user id & password',
+
+      }
+      return buildResponse(200, body);
+    }
+
+
+  }, (error) => {
+    console.error('Unable to create promoter details ', error);
+    const body = {
+      operation: 'VALIDATE_USER',
+      status: 'x06',
+      message: 'Please share valid user id & password',
+
+    }
+    return buildResponse(200, body);
+  })
+}
+
+async function changePwd(request,response){
+  //console.log("Request :"+JSON.stringify(request));
+  //console.log("Response :",JSON.stringify(response.body));
+  let responseData = JSON.parse(response.body);
+  //console.log("===>"+JSON.stringify(responseData));
+  const respparams = {
+    userId: responseData.data.Item.id,
+    lastName: responseData.data.Item.lastName,
+    firstName: responseData.data.Item.firstName,
+    storeId: responseData.data.Item.storeId
+
+  }
+  responseData.data.Item.pwd= request.password;
+  responseData.data.Item.pwdStatus="A";
+ 
+  const params = {
+    TableName: promotersTableName,
+    Item: responseData.data.Item
+  }
+  //console.log("update password1",JSON.stringify(params));
+  return await dynamodb.put(params).promise().then(() => {
+    //console.log("update password2");
+    const body = {
+      operation: 'UPDATE_PWD',
+      status: '00',
+      message: 'SUCCESS',
+      data: respparams
+    }
+    console.log("User :"+responseData.data.Item.id+" Password successfully updated...")
+    return buildResponse(200, body);
+  }, (error) => {
+    console.error('Unable to update  the user pwd details ', error,request.userId);
+    return buildResponse(404, "Bad Request");
+  })
+
+}
+
+async function wrongAttempt(reqbody,response){
+  let responseData = JSON.parse(response.body);
+  console.log(JSON.stringify(responseData))
+  responseData.data.Item.retryCt = responseData.data.Item.retryCt+1;
+  if(MAX_ATTEMP<=responseData.data.Item.retryCt)
+  {
+    responseData.data.Item.status="L";
+    
+  }
+  const params = {
+    TableName: promotersTableName,
+    Item: responseData.data.Item
+  }
+  return await dynamodb.put(params).promise().then(() => {
+    const body = {
+      operation: 'VALIDATE_USER',
+      status: 'x03',
+      message: 'Please share valid user id & password'
+      //data: reqbody
+    }
+    return buildResponse(200, body);
+  }, (error) => {
+    console.error('Unable to save the product details ', error);
+    return buildResponse(400, "Bad Request");
+  })
+
+}
+const isEmpty = (value) => (
+  value === undefined ||
+  value === null ||
+  (typeof value === 'object' && Object.keys(value).length === 0) ||
+  (typeof value === 'string' && value.trim().length === 0)
+)
+function buildResponse(statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PATCH",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Origin": "*",
+      "X-Requested-With": "*"
+    },
+    body: JSON.stringify(body)
+  }
+}
+function getMinutesBetweenDates(startDate, endDate) {
+  var diff = endDate.getTime() - startDate.getTime();
+  console.log("time diff :", diff)
+  return (diff / 60000);
+}
+function makePwd(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  console.log("Pwd:" + result);
+  //return crypto.createHash('md5').update(result).digest('hex');
+  return result;
+}
+function validateDateFormat(dateStr) {
+  //MM/DD/YYYY
+  var date_regex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
+  if (!(date_regex.test(dateStr))) {
+    console.log("dob wrong");
+    return false;
+
+  }
+  console.log("dob correct");
+  return true;
+}
+
+function sendSMS(mobileno,msg)
+{
+  console.log("Send Pwd via SMS");
+  try {
+    
+  } catch (error) {
+    console.log("Sorry , we are unable to send SMS for this customer ",mobileno);
+  }
+}
